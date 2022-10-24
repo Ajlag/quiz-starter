b3f9d1513da209bc658d2bffabc3be55bff027a1
unknown <ajlagudzevic1100@gmail.com>
2022-10-24 11:38:21 +0200

    Setting the timer

--- classes/Game.js
+++ classes/Game.js
@@ -2,6 +2,9 @@ class Game {
   constructor(questions) {
     this.questions = questions;
     this.currentQuestion = 0;
+    this.maxQuestion = 0;
+    this.clickPrevious = false;
+    this.maxQueCorrect = true;
   }
 
   shuffle(numOfShuffles) {
@@ -28,7 +31,7 @@ class Game {
     this.shuffle(100000);
     this.createQuestion();
     this.nextButton();
-    this.previousButton();
+    // this.previousButton();
     this.showScore();
     this.createTimer();
     this.restartButton();
@@ -49,10 +52,10 @@ class Game {
     nextB.addEventListener("click", () => this.next.call(this));
   }
 
-  previousButton() {
-    const prevButton = document.querySelector(".previous");
-    prevButton.addEventListener("click", () => this.previous.call(this));
-  }
+  // previousButton() {
+  //   const prevButton = document.querySelector(".previous");
+  //   prevButton.addEventListener("click", () => this.previous.call(this));
+  // }
 
   restartButton() {
     const restartBtn = document.querySelector(".restart");
@@ -62,12 +65,15 @@ class Game {
   restart() {
     this.shuffle(10000);
     this.currentQuestion = 0;
+    this.maxQuestion = 0;
+    this.maxQueCorrect = true;
     this.currentQuestionObj.attemptedAnswer = false;
     this.removeScore();
     this.changeAnswer();
     this.showScore();
     this.createQuestion();
-    // this.createTimer();
+    this.removeTimer();
+    this.createTimer();
   }
 
   changeAnswer() {
@@ -76,6 +82,7 @@ class Game {
   }
 
   next() {
+    this.removeTimer();
     this.createTimer();
 
     this.score.incrementScore(
@@ -99,6 +106,7 @@ class Game {
   }
 
   previous() {
+    this.clickPrevious = true;
     if (this.currentQuestion === 0) {
       return;
     }
@@ -107,8 +115,6 @@ class Game {
       return;
     }
 
-    this.createTimer();
-    console.log(this.currentQuestionObj.attemptedAnswer);
     this.changeAnswer();
     this.currentQuestion--;
 
@@ -116,7 +122,7 @@ class Game {
     const tacanOdg = pitanje.correct_answer;
 
     this.createQuestion();
-
+    console.log(this.currentQuestion, "click na previous");
     this.currentQuestionObj.attemptedAnswer = true;
 
     const divAnsw = document.querySelectorAll(".answ");
@@ -124,27 +130,37 @@ class Game {
   }
 
   createTimer() {
-    const selectTimer = document.querySelector("#timer");
+    const pTimer = document.createElement("p");
+    pTimer.innerText = "00:30";
+    pTimer.id = "timer";
+
+    const divTimer = document.querySelector(".group");
+    divTimer.appendChild(pTimer);
     let sec = 29;
     let timer = setInterval(() => {
       if (this.currentQuestionObj.attemptedAnswer) {
         sec = 29;
         return;
       }
-      selectTimer.innerHTML = "00:" + sec;
-      selectTimer.style.color = "white";
+      pTimer.innerHTML = "00:" + sec;
+      pTimer.style.color = "white";
       if (sec < 10) {
-        selectTimer.innerHTML = "00:0" + sec;
-        selectTimer.style.color = "red";
+        pTimer.innerHTML = "00:0" + sec;
+        pTimer.style.color = "red";
       }
       if (sec === 0) {
+        pTimer.innerHTML = "00:00";
         this.currentQuestionObj.attemptedAnswer = true;
-        selectTimer.innerHTML = "00:00";
 
         return;
       } else {
         sec--;
       }
     }, 1000);
+  }
+
+  removeTimer() {
+    const timer = document.getElementById("timer");
+    timer.remove();
   }
 }
